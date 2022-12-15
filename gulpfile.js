40bf6f0284bb8af213f1a31b739c814bbe47dca9
Diego Souza <disouzauva@gmail.com>
2022-12-15 02:06:43 -0300

    Desafio 3 finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const cleanCss = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
 const sourcemap = require("gulp-sourcemaps");
 
-const path = "./desafio-02";
+const path = "./desafio-03";
 const srcCSSPath = path + "/src/sass/**/*.scss";
 const dirCSSPath = path + "/dir/css";
 
